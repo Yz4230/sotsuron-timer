816acf1625c30ea317cd899f29e5c01cf8960e0d
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2025-01-29 17:14:29 +0900

    タイマーを実装

---
+++ src/app/_components/timer.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { differenceInSeconds, endOfDay, formatDate } from "date-fns";
+import { useEffect, useState } from "react";
+
+const DEADLINE = endOfDay("2025-01-31");
+
+export default function Timer() {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    function update() {
+      const now = new Date();
+      const remaining = differenceInSeconds(DEADLINE, now);
+      const hours = Math.floor(remaining / 3600);
+      const minutes = Math.floor((remaining % 3600) / 60);
+      const seconds = remaining % 60;
+
+      setText(`あと${hours}時間${minutes}分${seconds}秒`);
+    }
+
+    update();
+    const interval = setInterval(update, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center gap-2 sm:gap-4">
+      <div className="text-2xl sm:text-4xl">
+        卒論提出（{formatDate(DEADLINE, "MM/dd HH:mm")}）まで
+      </div>
+      <div className="text-3xl sm:text-5xl">{text}</div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Timer from "./_components/timer";
+
 export default function Home() {
   return (
     <div className="grid place-items-center min-h-svh">
-      <h1>こんにちは</h1>
+      <Timer />
     </div>
   );
 }
